471dc26a21c8e27a3c1dd28ffebe94361f15fb91
Nana Yu <nana@kwusen.com>
2024-08-29 13:43:38 -0700

    Correct helpers for mjs

--- src/helpers/ua-parser-helpers.mjs
+++ src/helpers/ua-parser-helpers.mjs
@@ -9,18 +9,33 @@
     AGPLv3 License */
 //////////////////////////////////////////////
 
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 
 import { CPU, OS, Engine } from '../enums/ua-parser-enums.mjs';
+import { UAParser } from '../main/ua-parser';
 
-const isAppleSilicon = (res) => res.os.is(OS.MACOS) && res.cpu.is(CPU.ARM);
+const getDeviceVendor = model => UAParser(`Mozilla/5.0 (Linux; Android 10; ${model}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36`).device.vendor;
 
-const isChromiumBased = (res) => res.engine.is(Engine.BLINK);
+const isAppleSilicon = res => res.os.is(OS.MACOS) && res.cpu.is(CPU.ARM);
 
-const isFrozenUA = (ua) => /^Mozilla\/5\.0 \((Windows NT 10\.0; Win64; x64|Macintosh; Intel Mac OS X 10_15_7|X11; Linux x86_64|X11; CrOS x86_64 14541\.0\.0|Fuchsia|Linux; Android 10; K)\) AppleWebKit\/537\.36 \(KHTML, like Gecko\) Chrome\/\d+\.0\.0\.0 (Mobile )?Safari\/537\.36/.test(ua);
+const isChromeFamily = res => res.engine.is(Engine.BLINK);
 
-export { 
+const isFrozenUA = ua => /^Mozilla\/5\.0 \((Windows NT 10\.0; Win64; x64|Macintosh; Intel Mac OS X 10_15_7|X11; Linux x86_64|X11; CrOS x86_64 14541\.0\.0|Fuchsia|Linux; Android 10; K)\) AppleWebKit\/537\.36 \(KHTML, like Gecko\) Chrome\/\d+\.0\.0\.0 (Mobile )?Safari\/537\.36/.test(ua);
+
+const isStandalonePWA = () => window && (window.matchMedia('(display-mode: standalone)').matches ||
+                                // iOS
+                                navigator.standalone ||
+                                // Android
+                                document.referrer.startsWith('android-app://') ||
+                                // Windows
+                                window.Windows ||
+                                /trident.+(msapphost|webview)\//i.test(navigator.userAgent) ||
+                                document.referrer.startsWith('app-info://platform/microsoft-store'));
+
+export {
+    getDeviceVendor,
     isAppleSilicon,
-    isChromiumBased,
-    isFrozenUA
+    isChromeFamily,
+    isFrozenUA,
+    isStandalonePWA,
 }
