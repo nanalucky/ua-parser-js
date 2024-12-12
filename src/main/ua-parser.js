62a4102a2b7dda6bce365ba4b5ce1e2c81ef6628
Faisal Salman <f@faisalman.com>
2024-12-12 07:29:56 +0700

    Add new device: Apple HomePod

--- src/main/ua-parser.js
+++ src/main/ua-parser.js
@@ -833,6 +833,8 @@
             ], [VENDOR, [TYPE, EMBEDDED]], [
             /(aeobc)\b/i                                                        // Echo Dot
             ], [MODEL, [VENDOR, AMAZON], [TYPE, EMBEDDED]], [
+            /(homepod).+mac os/i                                                // Apple HomePod
+            ], [MODEL, [VENDOR, APPLE], [TYPE, EMBEDDED]], [
             /windows iot/i
             ], [[TYPE, EMBEDDED]], [
 
@@ -891,7 +893,7 @@
             ], [[VERSION, strMapper, windowsVersionMap], [NAME, WINDOWS]], [
 
             // iOS/macOS
-            /ip[honead]{2,4}\b(?:.*os ([\w]+) like mac|; opera)/i,              // iOS
+            /[adehimnop]{4,7}\b(?:.*os ([\w]+) like mac|; opera)/i,             // iOS
             /(?:ios;fbsv\/|iphone.+ios[\/ ])([\d\.]+)/i,
             /cfnetwork\/.+darwin/i
             ], [[VERSION, /_/g, '.'], [NAME, 'iOS']], [
